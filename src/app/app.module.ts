a5aea31d406c195f874c58d573e10903a059211a
VotreNomGitHub <votre@email.com>
2025-03-27 10:08:48 +0100

    remove Firebase initialization and environment configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { initializeApp } from "firebase/app";
-import { environment } from '../environments/environment';
 
-// Initialize Firebase
-initializeApp(environment.firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent],
